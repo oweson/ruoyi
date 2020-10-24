a4b3e371cb24b2b497c66c28733083192399ff85
oweson <570347720@qq.com>
2020-10-24 16:21:30 +0800

    fix: 解决版本号不能存储小数的问题

--- ruoyi-admin/src/main/test/test/qq.java
+++ ruoyi-admin/src/main/test/test/qq.java
@@ -9,6 +9,7 @@ public class qq {
     private int age;
     private String name;
     public static void main(String[] args) {
+        System.out.println();
         System.out.println(2<<5);
     }
 
--- ruoyi-system/src/main/java/com/ruoyi/system/domain/BaiduDiskResourceSharing.java
+++ ruoyi-system/src/main/java/com/ruoyi/system/domain/BaiduDiskResourceSharing.java
@@ -18,13 +18,13 @@ public class BaiduDiskResourceSharing extends BaseEntity
     /** 主键 */
     private Long id;
 
+    /** 类型 */
+    @Excel(name = "类型")
+    private String type;
+
     /** 名字 */
     @Excel(name = "名字")
     private String name;
-
-    /** 描述 */
-    @Excel(name = "描述")
-    private String describe;
 
     /** 版本 */
     @Excel(name = "版本")
@@ -38,10 +38,6 @@ public class BaiduDiskResourceSharing extends BaseEntity
     @Excel(name = "大小")
     private Long size;
 
-    /** 类型 */
-    @Excel(name = "类型")
-    private String type;
-
     public void setId(Long id) 
     {
         this.id = id;
@@ -50,6 +46,15 @@ public class BaiduDiskResourceSharing extends BaseEntity
     public Long getId() 
     {
         return id;
+    }
+    public void setType(String type) 
+    {
+        this.type = type;
+    }
+
+    public String getType() 
+    {
+        return type;
     }
     public void setName(String name) 
     {
@@ -60,21 +65,12 @@ public class BaiduDiskResourceSharing extends BaseEntity
     {
         return name;
     }
-    public void setDescribe(String describe) 
-    {
-        this.describe = describe;
-    }
-
-    public String getDescribe() 
-    {
-        return describe;
-    }
-    public void setVersion(Double version) 
+    public void setVersion(Double version)
     {
         this.version = version;
     }
 
-    public Double getVersion() 
+    public Double getVersion()
     {
         return version;
     }
@@ -96,28 +92,19 @@ public class BaiduDiskResourceSharing extends BaseEntity
     {
         return size;
     }
-    public void setType(String type) 
-    {
-        this.type = type;
-    }
-
-    public String getType() 
-    {
-        return type;
-    }
 
     @Override
     public String toString() {
         return new ToStringBuilder(this,ToStringStyle.MULTI_LINE_STYLE)
             .append("id", getId())
+            .append("type", getType())
             .append("name", getName())
-            .append("describe", getDescribe())
+            .append("remark", getRemark())
             .append("version", getVersion())
             .append("resourcesUrl", getResourcesUrl())
             .append("size", getSize())
             .append("createTime", getCreateTime())
             .append("updateTime", getUpdateTime())
-            .append("type", getType())
             .toString();
     }
 }
