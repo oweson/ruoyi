bcd4fbcfa7364668f3d3933a7938026c8bdf5fb7
chengwanli <chengwanli@qq.com>
2021-03-07 23:27:25 +0800

    教训列表查询优化

---
+++ ruoyi-admin/src/main/test/dao/ManageFianceMapperTest.java
@@ -0,0 +1,33 @@
+package dao;
+
+import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
+import com.ruoyi.RuoYiApplication;
+import com.ruoyi.system.domain.ManageFinance;
+import com.ruoyi.system.mapper.ManageFinanceMapper;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import java.util.List;
+
+/**
+ * @author oweson
+ * @date 2021/3/5 21:06
+ */
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringBootTest(classes = RuoYiApplication.class)
+public class ManageFianceMapperTest  {
+    @Autowired
+    ManageFinanceMapper manageFinanceMapper;
+    @Test
+    public void demo(){
+        PageHelper.startPage(1,10);
+        List<ManageFinance> manageFinances = manageFinanceMapper.selectManageFinanceList(new ManageFinance());
+        PageInfo<ManageFinance> manageFinancePageInfo = new PageInfo<>(manageFinances);
+        System.out.println(manageFinancePageInfo);
+    }
+}
